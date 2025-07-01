acfb9286fbb2d91816edd3a6f45c3fccf8346f4e
Ryan Fisher <ryanfisher099@gmail.com>
2025-07-02 01:45:19 +1000

    Added build to ignore

--- build/thirdparty/SDL/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-prerelease-2.29.2-377-g1edaad172 (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-prerelease-2.29.2-377-g1edaad172"
-#endif
